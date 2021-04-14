00c9fea9c386cf0be6337f4ed0a4737bfde70b2a
Alexander <alex030t@stud.kea.dk>
2021-04-14 12:02:42 +0200

    Fixed the css thingy

--- src/main/java/com/example/demo/controllers/UIController.java
+++ src/main/java/com/example/demo/controllers/UIController.java
@@ -10,49 +10,42 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+
 @Controller
 public class UIController {
-    private HttpSession session;
     private Wishlist wishlist = new Wishlist();
+    private HttpSession session;
 
     @GetMapping(value = "/login")
-    public String renderLoginPage(){
-
+    public String renderLoginPage(HttpServletRequest request) {
+        session = request.getSession();
         return "login.html";
     }
 
-    @PostMapping(value = "/checkLogin")
-    public String checkLogin(@RequestParam("username") String username, @RequestParam("password") String password, HttpServletRequest request) {
-        session = request.getSession();
-        session.setAttribute("user-logged-in","false");
-
-        if(session.getAttribute("user-logged-in").equals("true")){
-            return "redirect:/userpage";
-        }
-
-        if (wishlist.checkInformation(username,password)) {
-            session.setAttribute("user-logged-in", "true");
+    @PostMapping(value = "/check-login")
+    public String checkLogin(@RequestParam("username") String username, @RequestParam("password") String password) {
+        if (wishlist.checkInformation(username, password)) {
+            session.setAttribute("logged-in",true);
             return "redirect:/userpage";
         } else {
-            session.setAttribute("user-logged-in", "false");
             return "redirect:/login";
         }
     }
 
     @GetMapping(value = "/userpage")
-    public String renderUserpage(Model user) {
-        if(session.getAttribute("user-logged-in").equals("true")){
-            user.addAttribute("username",wishlist.getUsername());
-            user.addAttribute("wishlist",wishlist.getWishlist());
-            return "userpage.html";
+    public String renderUserpage(Model user,HttpServletRequest request) {
+        boolean loggedIn = (boolean) session.getAttribute("logged-in");
+        if (!loggedIn) {
+            return "redirect:/login";
         }
-        else{
-            return "redirect:/login.html";
-        }
+
+        user.addAttribute("username", wishlist.getUsername());
+        user.addAttribute("wishlist", wishlist.getWishlist());
+        return "userpage.html";
     }
 
     @GetMapping("/frontpage")
-    public String frontpage(){
+    public String frontpage() {
         return "frontpage.html";
     }
 }
