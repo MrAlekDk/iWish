d3d81c1db31d06919d32dced3ea0f85904c90c50
Alexander <alex030t@stud.kea.dk>
2021-04-16 11:52:42 +0200

    Added function so it's not possible to make a wish when not logged in

--- src/main/java/com/example/demo/controllers/IWishController.java
+++ src/main/java/com/example/demo/controllers/IWishController.java
@@ -4,11 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class IWishController {
 
     @GetMapping(value = "/createWish")
-    public String renderWishCreatorPage(){
+    public String renderWishCreatorPage(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        if(session.getAttribute("username") ==null){
+            return "redirect:/frontpage";
+        }
         return "wishcreator.html";
     }
 
